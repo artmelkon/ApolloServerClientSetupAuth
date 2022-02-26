186c2ddb1708f74b9fd7d59bfb0b9c3e08a39f44
Arthur Melkonyan <arthur@artmelkon.me>
2022-02-26 17:35:37 +0200

    update homepage

--- client/src/pages/homepage.js
+++ client/src/pages/homepage.js
@@ -1,4 +1,4 @@
-import React, {Fragment, useContext} from 'react';
+import React, { useContext} from 'react';
 import {Container} from '@mui/material';
 
 import { AuthContext } from "../context/auth-context";
